174f1b2b0d8d5bd0c82e70d5520f2438d3b4ff57
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-12-07 01:42:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  /* 
+    Step 1: Start
+    Step 2: Declare variable re
+    Step 3: specify string to lowercase
+    Step 4: Declare variable len to represent string length
+    Step 5; Iterate over each item
+    Step 6: if word iterated !== word length
+          return false;
+          else
+          return true; 
+    Step 7: Stop
+  */
+  var re = /[^A-Za-z0-9]/g;
+ word = word.toLowerCase().replace(re, '');
+ var len = word.length;
+ for (var i = 0; i < len/2; i++) {
+   if (word[i] !== word[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
 }
+isPalindrome("")
 
 /* 
   Add your pseudocode here
+ 
+  initialize a variable called re
+      specify our string toLowercase
+      iniialize another variable len to check the lenght of the string
+      iterate over each item in the variable
+      if string is not equal to string length expected
+          return result
 */
 
 /*
